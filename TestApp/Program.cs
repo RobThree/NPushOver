3be1eca0c2eaaa195dab6f3ab8880e2d5493ef46
RobThree <rob@devcorner.nl>
2015-06-18 23:30:53 +0200

    * More examples in TestApp

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NPushover;
 using NPushover.RequestObjects;
+using System;
 using System.Threading.Tasks;
 
 namespace TestApp
@@ -10,11 +11,29 @@ namespace TestApp
         {
             var po = new Pushover("[APPLICATION-KEY-HERE]");
 
-            var msg = Message.Create(Priority.Normal, "Hello world!");
+            // Quick message:
+            var msg1 = Message.Create(Priority.Normal, "Hello world!");
+            var sendtask1 = po.SendMessageAsync(msg1, "[RECIPIENT-ID-HERE]");
 
-            var sendtask = po.SendMessageAsync(msg, "[RECIPIENT-ID-HERE]");
+            // More comprehensive:
+            var msg2 = Message.Create(Priority.High, "The roof!", "The roof is on fire!", false, Sounds.Siren);
+            msg2.RetryOptions = new RetryOptions { RetryEvery = TimeSpan.FromSeconds(30), RetryPeriod = TimeSpan.FromHours(3) };
+            msg2.SupplementaryUrl = new SupplementaryURL { Uri = new Uri("http://robiii.me"), Title = "Awesome dude!" };
+            var sendtask2 = po.SendMessageAsync(msg2, "[RECIPIENT-ID-HERE]", "[DEVICE-ID-HERE]");
 
-            Task.WaitAll(sendtask);
+
+            // Send our messages            
+            Task.WaitAll(sendtask1, sendtask2);
+
+            // Other examples:
+            Task.WaitAll(
+                po.ValidateUserOrGroupAsync("[USER-ID-HERE]"),
+                po.CancelReceiptAsync("[RECEIPT-ID-HERE]"),
+                po.ListSoundsAsync(),
+                po.LoginAsync("[EMAIL-HERE]", "[PASSWORD-HERE]"),
+                po.ListMessagesAsync("[SECRET-HERE]", "[DEVICE-ID-HERE]")
+            );
+            // ...etc.
         }
     }
 }
